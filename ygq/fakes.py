eeaf87e2204729072cfe00d07d6affcb61186057
chichengfengxue <75111874+chichengfengxue@users.noreply.github.com>
2022-06-03 00:43:13 +0800

    Update fakes.py

--- ygq/fakes.py
+++ ygq/fakes.py
@@ -181,3 +181,9 @@ def fake_message(count=200):
         )
     db.session.add(message)
     db.session.commit()
+    
+def fake_room(count=1):
+    for i in range(count):
+        room = Room()
+    db.session.add(room)
+    db.session.commit()
