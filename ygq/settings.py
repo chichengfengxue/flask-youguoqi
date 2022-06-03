ff920a67e3966df32ea7ea83ebdba374beb7a888
chichengfengxue <75111874+chichengfengxue@users.noreply.github.com>
2022-06-03 14:03:31 +0800

    奇怪的配置

--- ygq/settings.py
+++ ygq/settings.py
@@ -87,21 +87,18 @@ class DevelopmentConfig(BaseConfig):
 #     SQLALCHEMY_DATABASE_URI = \
 #         prefix + os.path.join(basedir, 'data-dev.db')
 #     REDIS_URL = "redis://localhost"
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',
-                                    prefix + os.path.join(basedir, 'data.db'))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     # TESTING = True
     # WTF_CSRF_ENABLED = False
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///'  # in-memory database
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',
-                                    prefix + os.path.join(basedir, 'data.db'))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
 class ProductionConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL',
-                                        prefix + os.path.join(basedir, 'data.db'))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
 config = {
